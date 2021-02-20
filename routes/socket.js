00615a74a88a496cf1abc246b84c6565e7c101ea
e <chengyang@linkdoc.com>
2021-02-20 14:46:57 +0800

    加上端口说话

--- routes/socket.js
+++ routes/socket.js
@@ -1,60 +1,63 @@
 const {Subject}=require('rxjs')
 const ws=require('ws')
-//port需要暴露出来给下面的router使用
-let port=3001
-let commonData='系统欢迎你进入聊天室'
+//初始化在线人数和端口
 let onlineNumber=0
+const firstPort=3001
+
 const sendSubject=new Subject()
 const connectSubject=new Subject()
-connectSubject.subscribe(val=>{
+//后端监听端口以连接客户端发来的websocket请求
+connectSubject.subscribe(port=>{
     const io=new ws.Server({
         port:port,
     })
-    console.log(io.on);
-    
     io.on("connection", (wsObj)=>{
         onlineNumber++
         const date=new Date().toLocaleString('chinese',{hour12:false})
+        //客户端第一次进来
         wsObj.send(JSON.stringify({
-            text:commonData,
+            text:'欢迎你进入聊天室',
             onlineNumber,
             date,
+            port:firstPort,
         }))
+ 
         sendSubject.subscribe(data=>{
             wsObj.send(JSON.stringify(data))
         })
+        //在线人数的改变
         sendSubject.next({onlineNumber})
-        wsObj.on('message',(data)=>{
-            commonData=data
+
+        wsObj.on('message',(text)=>{
+            //消息的改变
             sendSubject.next({
-                text:data,
+                text,
                 onlineNumber,
                 date,
+                port,
             })
         })
         wsObj.on('close',()=>{
             onlineNumber--
             sendSubject.next({
-                text:commonData,
                 onlineNumber,
-            })
+            });
         })
-        if(port>65000){
-            port=3001
-        }else{
-            port++
-        }
-        connectSubject.next(port)
     })
 })
-connectSubject.next(3001)
-
 //路由相关
 const express = require('express')
 const router = express.Router()
+let port=firstPort
 router.get('/', function(req, res, next) {
+    connectSubject.next(port);
     res.send({
         port:port
     })
-});
+    if(port>65000){
+        port=firstPort
+    }else{
+        port++
+    }
+})
 module.exports = router
