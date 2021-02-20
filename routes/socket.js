341d6be0b88d2848432da12863d205d5076bdb78
e <chengyang@linkdoc.com>
2021-02-20 15:00:45 +0800

    整理代码，将端口和人数放在后面

--- routes/socket.js
+++ routes/socket.js
@@ -1,9 +1,5 @@
 const {Subject}=require('rxjs')
 const ws=require('ws')
-//初始化在线人数和端口
-let onlineNumber=0
-const firstPort=3001
-
 const sendSubject=new Subject()
 const connectSubject=new Subject()
 //后端监听端口以连接客户端发来的websocket请求
@@ -19,7 +15,7 @@ connectSubject.subscribe(port=>{
             text:'欢迎你进入聊天室',
             onlineNumber,
             date,
-            port:firstPort,
+            port:'系统',
         }))
  
         sendSubject.subscribe(data=>{
@@ -45,10 +41,15 @@ connectSubject.subscribe(port=>{
         })
     })
 })
+
+//初始化端口和在线人数
+const firstPort=3001
+let port=firstPort
+let onlineNumber=0
+
 //路由相关
 const express = require('express')
 const router = express.Router()
-let port=firstPort
 router.get('/', function(req, res, next) {
     connectSubject.next(port);
     res.send({
@@ -60,4 +61,5 @@ router.get('/', function(req, res, next) {
         port++
     }
 })
+
 module.exports = router
