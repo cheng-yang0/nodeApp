58705317e35cf5728225dd69e2d6137fd847b32e
e <chengyang@linkdoc.com>
2021-02-22 11:34:56 +0800

    media.js文件增加

---
+++ routes/media.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path=require('path')
+const os=require('os')
+const multiparty=require('multiparty')
+const fs=require('fs')
+const jwt=require('jsonwebtoken')
+const router = express.Router();
+const mongoose=require('../mongoose.js');
+const { multiply } = require('lodash');
+const UserSchema=new mongoose.Schema({
+	userName:String,
+	password:String
+})
+router.post('/', async (req, res)=>{ 
+    const form=new multiparty.Form()
+    form.encoding = 'utf-8';
+    form.uploadDir=path.resolve('..')
+    form.parse(req, function (err, fields, files) {
+        try {            
+            const inputFile = files.file[0];
+            const originalFilename=inputFile.originalFilename
+            const newPath=path.resolve(`${form.uploadDir}/${inputFile.originalFilename}`)
+            fs.renameSync(inputFile.path, newPath);
+            const ip=os.type()=='Linux' ? '8.131.57.124' : 'localhost'
+            res.send({data:`http://${ip}:3000/${originalFilename}`})
+            
+        } catch (err) {
+            res.send({ err: "上传失败！" });
+        };
+    })
+});
+module.exports = router;
+
